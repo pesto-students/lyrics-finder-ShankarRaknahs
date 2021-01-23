5225e3057e52ea49d28c2be1d76de95985c70dd5
ShankarRaknahs <shankarsachin2u@gmail.com>
2021-01-23 18:35:41 +0530

    add wireframe reference(figma) for review

---
+++ app/lyrics-finder/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <a
+          className='App-link'
+          href='https://www.figma.com/proto/WAmXdWdTjyXPh1oZcRSQsL/Mobile-Wireframe-UI-Kit-(Community)?node-id=8%3A44&scaling=min-zoom'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Click here to check the mock screens
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
