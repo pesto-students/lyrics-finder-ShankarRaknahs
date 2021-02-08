da5e7abd11dc3b1d0212b457456ec733323d38e4
ShankarRaknahs <shankarsachin2u@gmail.com>
2021-02-08 11:57:51 +0530

    Add test scripts for Profile, Header and Footer component

---
+++ app/lyrics-finder/src/components/Header/__tests__/Header.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import renderer from 'react-test-renderer';
+
+// import { render } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import Header from '../Header';
+
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import toJSON from 'enzyme-to-json';
+Enzyme.configure({ adapter: new Adapter() });
+const { mount } = Enzyme;
+
+describe('<Header />', () => {
+  it('render correctly (snapshot)', () => {
+    const app = { firstName: 'Lyrics', secondName: 'Finder' };
+    const tree = renderer.create(<Header {...app} />).toJSON();
+    expect(toJSON(tree)).toMatchSnapshot();
+  });
+
+  it('render correctly using Enzyme (snapshot)', () => {
+    const app = { firstName: 'Lyrics', secondName: 'Finder' };
+    const tree = mount(<Header {...app} />);
+    expect(toJSON(tree)).toMatchSnapshot();
+  });
+});
